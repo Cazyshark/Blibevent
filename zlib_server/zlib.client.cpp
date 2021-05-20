e445949a194f548524a2a0c239e2b7fc94d31761
Sir.Wang <Cazyshark@gmail.com>
2021-05-20 15:50:15 +0800

    final

--- zlib_server/zlib.client.cpp
+++ zlib_server/zlib.client.cpp
@@ -3,27 +3,80 @@
 #include<event2/buffer.h>
 #include<event2/bufferevent.h>
 #include<string.h>
+#include<zlib.h>
 #include<event2/listener.h>
-#define FILEPATHE "1.png"
+#define FILEPATHE "1.txt"
 using namespace std;
 struct  ClientStatus{
     FILE *fp = 0;
     bool filend = false;
+    z_stream *z_output = 0;
+    int readNum =0;
+    int writeNum = 0;
+    bool startSend = false;
+    ~ClientStatus(){
+        delete z_output;
+        z_output = 0;
+    }
 };
 
 bufferevent_filter_result c_filter_out(evbuffer *s,evbuffer *d,ev_ssize_t limit,bufferevent_flush_mode mode,void* arg){
-    cout<<"client filter out"<<endl;
-    char data[1024] = {0};
-    int len = evbuffer_remove(s,data,sizeof(data));
-    evbuffer_add(d,data,len);
+    ClientStatus *status = (ClientStatus*)arg;
+    if(status->startSend=false){
+        char data[1024] = {0};
+        int len = evbuffer_remove(s,data,sizeof(data));
+        evbuffer_add(d,data,len);
+        return BEV_OK;
+    }
+    //start zlib
+    //step 1
+    evbuffer_iovec v_in[1];
+    int n = evbuffer_peek(s,-1,0,v_in,1);
+    if(n<=0){
+        return BEV_NEED_MORE;
+    }
+    //step 2
+    z_stream *p =status->z_output;
+    if(!p){
+        return BEV_ERROR;
+    }
+    //step 3
+    p->avail_in =v_in[0].iov_len;
+    p->next_in  =(Byte*)v_in[0].iov_base;
+    //step 4
+    evbuffer_iovec v_out[1];
+    evbuffer_reserve_space(d,4096,v_out,1);
+    //step 5
+    p->avail_out = v_out[0].iov_len;
+    p->next_out = (Byte*)v_out[0].iov_base;
+    //step 6
+    int re = deflate(p,Z_SYNC_FLUSH);
+    if (re != Z_OK)
+    {
+        cerr<<"deflate failed"<<endl;
+    }
+    //step 7
+    int nread = v_in[0].iov_len - p->avail_in;
+    //step 8
+    int nwrite = v_out[0].iov_len - p->avail_out;
+    //step 9
+    evbuffer_drain(s,nread); 
+    v_out[0].iov_len = nwrite;
+    evbuffer_commit_space(d,v_out,1);
+    cout<<"client nread= "<<nread<<"  client nwrite= "<<nwrite<<endl;
+    status->readNum += nread;
+    status->writeNum += nwrite;
+    //cout<<"client filter out"<<endl;
     return BEV_OK;
 }
 void c_read_cb(bufferevent *bev,void *arg){
-    cout<<"client_read_cb"<<endl;
+   // cout<<"client_read_cb"<<endl;
+    ClientStatus *status = (ClientStatus*)arg;
     char data[1024] = {0};
     int len = bufferevent_read(bev,data,sizeof(data)-1);
     if(strcmp(data,"ok")==0){
-        cout<<"client data:"<<data<<endl;
+        //cout<<"client data:"<<data<<endl;
+        status->startSend = true;
         bufferevent_trigger(bev,EV_WRITE,0);
     }else{
         bufferevent_free(bev);
@@ -48,10 +101,12 @@ void c_write_cb(bufferevent *bev,void *arg){
         return;
     }
     
-    cout<<"client_write_cb"<<endl;
+    //cout<<"client_write_cb"<<endl;
     char data[1024] = {0};
     int len = fread(data,1,sizeof(data),fp);
     if(len<=0){
+        cout<<"client read:"<<status->readNum<<endl;
+        cout<<"Client send:"<<status->writeNum<<endl;
         fclose(fp);
         status->filend=true;
         bufferevent_flush(bev,EV_WRITE,BEV_FINISHED);
@@ -60,25 +115,27 @@ void c_write_cb(bufferevent *bev,void *arg){
     bufferevent_write(bev,data,len);
 }
 void c_event_cb(bufferevent *bev,short sin,void *arg){
-    cout<<"client_event"<<endl;   
+    //cout<<"client_event"<<endl;   
 }
 void client_event_cb(bufferevent*be, short events,void *arg){
-    cout<<"client_event_cb"<<endl;
+    //cout<<"client_event_cb"<<endl;
     if(events & BEV_EVENT_CONNECTED){
         cout<<"BEV_EVENT_CONNECTED"<<endl;
         bufferevent_write(be,FILEPATHE,strlen(FILEPATHE));
-        bufferevent * bev_filter = bufferevent_filter_new(be,0,c_filter_out,BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS,0,0);
         FILE* fp = fopen(FILEPATHE,"rb");
         ClientStatus *status =new ClientStatus();
         status->fp =fp;
         status->filend = false;
+        status->z_output = new z_stream();
+        deflateInit(status->z_output,Z_DEFAULT_COMPRESSION);
         cout<<"open file: "<<FILEPATHE<<endl;
+        bufferevent * bev_filter = bufferevent_filter_new(be,0,c_filter_out,BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS,0,status);
         bufferevent_setcb(bev_filter,c_read_cb,c_write_cb,c_event_cb,status);
         bufferevent_enable(bev_filter,EV_READ|EV_WRITE);
     }    
 }
 void Client(event_base* base){
-    cout<<"begin Client"<<endl;
+    //cout<<"begin Client"<<endl;
     sockaddr_in sin;
     memset(&sin,0,sizeof(sin));
     sin.sin_family = AF_INET;
--- zlib_server/zlib.server.cpp
+++ zlib_server/zlib.server.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<string.h>
 #include<string>
+#include<zlib.h>
 #include<event2/buffer.h>
 #include<event2/listener.h>
 #include<event2/bufferevent.h>
@@ -10,15 +11,58 @@ struct Status
 {
    bool start = false;
    FILE *fp = 0;
+   z_stream* p;
+   int recvNum  = 0;
+   int writeNum = 0;
 };
 
 bufferevent_filter_result filter_in(evbuffer *s,evbuffer *d,ev_ssize_t limit,bufferevent_flush_mode mode,void* arg){
-    char data[1024] = {0};
-    int len = evbuffer_remove(s,data,sizeof(data)-1);
-    cout<<"server recv:"<<data<<endl;
-    evbuffer_add(d,data,len);
+    Status *status = (Status*) arg;
+    if(!status->start){
+        char data[1024] = {0};
+        int len = evbuffer_remove(s,data,sizeof(data)-1);
+        cout<<"server recv:"<<len<<endl;
+        evbuffer_add(d,data,len);
+        return BEV_OK;
+    }
+    evbuffer_iovec v_in[1];
+    int n = evbuffer_peek(s,-1,nullptr,v_in,1);
+    if(n<=0){
+        return BEV_NEED_MORE;
+    }
+    z_stream *p =status->p;
+    if(!p){
+        return BEV_ERROR;
+    }
+    //step 3
+    p->avail_in =v_in[0].iov_len;
+    p->next_in  =(Byte*)v_in[0].iov_base;
+    //step 4
+    evbuffer_iovec v_out[1];
+    evbuffer_reserve_space(d,4096,v_out,1);
+    //step 5
+    p->avail_out = v_out[0].iov_len;
+    p->next_out = (Byte*)v_out[0].iov_base;
+    //step 6
+    int re = inflate(p,Z_SYNC_FLUSH);
+    if (re != Z_OK)
+    {
+        cerr<<"deflate failed"<<endl;
+    }
+    //step 7
+    int nread = v_in[0].iov_len - p->avail_in;
+    //step 8
+    int nwrite = v_out[0].iov_len - p->avail_out;
+    //step 9
+    evbuffer_drain(s,nread); 
+    v_out[0].iov_len = nwrite;
+    evbuffer_commit_space(d,v_out,1);
+    //cout<<"server nread= "<<nread<<"  server nwrite= "<<nwrite<<endl;
+    status->recvNum += nread;
+    status->writeNum +=  nwrite;
     return BEV_OK;
 }
+
 void read_cb(bufferevent *bev,void* arg){
     Status *status = (Status*) arg;
 
@@ -32,18 +76,19 @@ void read_cb(bufferevent *bev,void* arg){
         status->start = true;
         return;
     }
-    do{char data[1024] = {0};
-    int len = bufferevent_read(bev,data,sizeof(data));
-    if(len>=0){
-        fwrite(data,1,len,status->fp);
-        fflush(status->fp);
-    }
+    do{
+        char data[1024] = {0};
+        int len = bufferevent_read(bev,data,sizeof(data));
+        if(len>=0){
+            fwrite(data,1,len,status->fp);
+            fflush(status->fp);
+        }
     }while(evbuffer_get_length(bufferevent_get_input(bev))>0);
     
     
 }
 void write_cb(bufferevent *bev,void* arg){
-    cout<<"server_write_cb"<<endl;
+    //cout<<"server_write_cb"<<endl;
 }
 void event_cb(bufferevent *bev,short events,void* arg){
     cout<<"server_event_cb"<<endl;
@@ -51,6 +96,8 @@ void event_cb(bufferevent *bev,short events,void* arg){
     if(events & BEV_EVENT_EOF){
         cout<<"server event BEV_EVENT_EOF"<<endl;
         if(status->fp){
+            cout<<"Server recv:"<<status->recvNum<<endl;
+            cout<<"Server write:"<<status->writeNum<<endl;
             fclose(status->fp);
             status->fp = 0;
         }
@@ -58,17 +105,19 @@ void event_cb(bufferevent *bev,short events,void* arg){
     }
 }
 void listen_cb(struct evconnlistener *e,evutil_socket_t s,struct sockaddr *a,int socklen,void* arg){
-    cout<<"server_listen_cb"<<endl;
+    //out<<"server_listen_cb"<<endl;
     event_base * base = (event_base*)arg;
     bufferevent* bev = bufferevent_socket_new(base,s,BEV_OPT_CLOSE_ON_FREE);
     Status *status = new Status();
-    bufferevent *bev_fileter = bufferevent_filter_new(bev,filter_in,0,BEV_OPT_CLOSE_ON_FREE,0,0);
+    status->p = new z_stream();
+    inflateInit(status->p);
+    bufferevent *bev_fileter = bufferevent_filter_new(bev,filter_in,0,BEV_OPT_CLOSE_ON_FREE,0,status);
     bufferevent_setcb(bev_fileter,read_cb,write_cb,event_cb,status);
     bufferevent_enable(bev_fileter,EV_READ|EV_WRITE);
 
 }
 void Server(event_base* base){
-    cout<<"Begin Server"<<endl;
+    //cout<<"Begin Server"<<endl;
     sockaddr_in sin;
     memset(&sin,0,sizeof(sin));
     sin.sin_port=htons(5000);
